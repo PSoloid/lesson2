aa5dc0a96c1975b08403929922f1a46084043e79
Alex Kolobov <kolobov_a@ukr.net>
2015-06-23 21:35:28 +0300

    fffff

---
+++ src/com/javaHellel/PSoloid/Cicle.java
@@ -0,0 +1,28 @@
+package com.javaHellel.PSoloid;
+
+/**
+ * Created by ITHILLEL7 on 23.06.2015.
+ */
+public class Cicle {
+    public static void main(String[] args) {
+        int i =0;
+        while (i<10){
+            i++;
+            System.out.println(i);
+        }
+        do{
+            System.out.println(i);
+            int q++;
+        }
+
+        while(q=3);
+
+
+        for(int i = 0;i<10;i++){
+            System.out.println(i);
+            continue;
+            break;
+        }
+
+    }
+}
